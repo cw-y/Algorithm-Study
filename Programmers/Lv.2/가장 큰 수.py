01e9b363dfaaf1549385429639f2c1b80af5e6b0
cw-y <rina0405@naver.com>
2023-10-24 12:53:28 +0900

    10/24

---
+++ Programmers/Lv.2/가장 큰 수.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    numbers = [str(i) for i in numbers]
+    numbers.sort(key=lambda x:x*3, reverse=True)
+    answer =  ''.join(numbers)
+
+    if answer[0] == '0':
+        return '0'
+    else:
+        return answer
